96898020e3d19af4b325d685be748e5ed0c17748
Hellshi <hcslimaa@gmail.com>
2021-06-25 12:12:06 -0300

    Initialized the validations in the input

---
+++ src/components/SimpleInput.js
@@ -0,0 +1,64 @@
+import { useState, useRef } from "react";
+const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+  const [placeholder, setPlaceHolder] = useState("");
+  const [EnteredNameIsValid, SetEnteredNameIsValid] = useState(true);
+  const [focus, setFocus] = useState(false);
+
+  const handleOnFocus = () => {
+    setFocus(true);
+    if (enteredName.trim() === "") {
+      setPlaceHolder("Type something");
+      SetEnteredNameIsValid(false);
+      return;
+    }
+  };
+
+  const handleInput = (e) => {
+    setEnteredName(e.target.value);
+    SetEnteredNameIsValid(true);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const enteredValue = nameInputRef.current.value;
+    if (enteredName.trim() === "") {
+      setPlaceHolder("Type something");
+      SetEnteredNameIsValid(false);
+      return;
+    }
+    console.log(enteredValue);
+    setPlaceHolder("");
+    setEnteredName("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div
+        className={
+          EnteredNameIsValid
+            ? "form-control"
+            : `form-control-${EnteredNameIsValid}`
+        }
+      >
+        <label htmlFor="name">Your Name</label>
+        <input
+          onBlur={handleOnFocus}
+          ref={nameInputRef}
+          placeholder={placeholder}
+          onChange={handleInput}
+          value={enteredName}
+          type="text"
+          id="name"
+        />
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
